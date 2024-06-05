8020672ea6eccbfd0b4f3915b79004acd7a31ed5
BounAbdallah <bigmabo61345@gmail.com>
2024-06-05 03:29:03 +0000

    Ajout image

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,34 @@
 <?php
-
-use App\Models\Bien;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BienController;
-use App\Http\Controllers\CommentaireController;
-use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BienController;
 use App\Http\Controllers\DashboardController;
-
-Route::get('/', [BienController::class, 'index']);
-
+use App\Http\Controllers\CommentaireController;
 
 
-// Routes pour les users
-// cette route nous affiche le formulaire dde connexion
+Route::get('/', [BienController::class, 'index'])->name('biens.index');
+Route::get('biens', [BienController::class, 'index'])->name('biens.index');
+Route::get('biens/{bien}', [BienController::class, 'show'])->name('biens.show');
 
+// Routes for comments
+Route::post('/biens/{bien}/commentaires', [CommentaireController::class, 'store'])->name('commentaires.store');
+Route::get('/commentaires/{commentaire}/edit', [CommentaireController::class, 'edit'])->name('commentaires.edit');
+Route::put('/commentaires/{commentaire}', [CommentaireController::class, 'update'])->name('commentaires.update');
+Route::delete('/commentaires/{commentaire}', [CommentaireController::class, 'destroy'])->name('commentaires.destroy');
+
+// Routes for user authentication
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
-
-// Cette route gére le traitement de la connexion de l'utilisateur
 Route::post('login', [AuthController::class, 'login']);
-
-// Cette route fais la déconnexion de l'utilisateur
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-
-// Cette route affiche le formulaire d'iscription d'un utilisateur
 Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
-
-// Cette route gére le traitement de l'enregistrement plus validation des information avant d'enregistrer dans la base des  données
 Route::post('register', [AuthController::class, 'register']);
 
-
-
-// Cette route joue le rôle de protecteur du dashboard avec le middleware qui protege le dashboard par une authentification obligatoire avant d'y acceder
-
-
+// Routes protected by authentication
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
-    Route::get('/biens/create', [DashboardController::class, 'create'])->name('create_bien');
-    Route::post('/biens', [DashboardController::class, 'store'])->name('store_bien');
+    Route::get('/create', [DashboardController::class, 'create'])->name('create_bien');
+    Route::post('/biens', [DashboardController::class, 'store'])->name('store_bien'); // This is for creating new Bien
     Route::get('/biens/{id}/edit', [DashboardController::class, 'edit'])->name('edit_bien');
-    Route::put('/biens/{id}', [DashboardController::class, 'update'])->name('update_bien');
+    Route::put('/biens/{id}', [DashboardController::class, 'update'])->name('update_bien'); // This is for updating existing Bien
     Route::delete('/biens/{id}', [DashboardController::class, 'delete'])->name('delete_bien');
 });
-Route::resource('biens', BienController::class);
