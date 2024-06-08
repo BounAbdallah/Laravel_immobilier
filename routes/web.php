55b95a49fee7f118b19a0765832cf273772c8740
BounAbdallah <bigmabo61345@gmail.com>
2024-06-08 23:38:27 +0000

    resolution de bug pour la route de modification d'un bien

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,15 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CommentaireController;
 
 
+Route::get('/', [BienController::class, 'index'])->name('biens.index');
+Route::get('biens', [BienController::class, 'index'])->name('biens.index');
+Route::get('biens/{bien}', [BienController::class, 'show'])->name('biens.show');
 
-
-
-Route::get('/', [BienController::class, 'index']);
-
-
-// Routes pour les biens
-Route::get('/biens', [BienController::class, 'index']);
-Route::get('/biens/ajouter', [BienController::class, 'ajouter']);
-Route::post('/biens/traitement', [BienController::class, 'traitement']);
-Route::get('/update-bien/{id}',[BienController::class,'update']);
-Route::post('/update-bien/{id}',[BienController::class,'updateTraitement']);
-Route::get('/bien/delete/{id}',[BienController::class,'delete']);
-
-
-// Routes pour les commentaires
+// Routes for comments
 Route::post('/biens/{bien}/commentaires', [CommentaireController::class, 'store'])->name('commentaires.store');
 Route::get('/commentaires/{commentaire}/edit', [CommentaireController::class, 'edit'])->name('commentaires.edit');
 Route::put('/commentaires/{commentaire}', [CommentaireController::class, 'update'])->name('commentaires.update');
 Route::delete('/commentaires/{commentaire}', [CommentaireController::class, 'destroy'])->name('commentaires.destroy');
-
-
-Route::resource('biens', BienController::class);
 
 // Routes for user authentication
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
@@ -36,7 +22,6 @@ Route::post('login', [AuthController::class, 'login']);
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [AuthController::class, 'register']);
-
 
 // Routes protected by authentication
 Route::middleware('auth')->group(function () {
@@ -47,6 +32,3 @@ Route::middleware('auth')->group(function () {
     Route::put('/biens/{id}', [DashboardController::class, 'update'])->name('update_bien'); // This is for updating existing Bien
     Route::delete('/biens/{id}', [DashboardController::class, 'delete'])->name('delete_bien');
 });
-
-
-
